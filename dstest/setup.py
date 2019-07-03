7240cb67f74730baa157ce2ad4e63443d11ec181
Clement Wang <clwan@microsoft.com>
2019-07-03 16:53:08 +0800

    first version

---
+++ dstest/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dstest",
+    version="0.0.1",
+    description="dstest",
+    packages=["dstest"],
+    author="Clement Wang",
+    license="MIT",
+    include_package_data=True,
+)
---
+++ dstest/test.py
@@ -0,0 +1,3 @@
+import dstest.echo as echo
+echo.main()
+#python test.py --input_data_path ./input --out_data_path ./output --column NewColumn
